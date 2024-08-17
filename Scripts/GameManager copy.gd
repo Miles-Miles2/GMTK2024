34fb6f17d2b4e6e4eb763938ac9c7684e382ffab
Andy <andyqulam@gmail.com>
2024-08-17 14:47:15 -0700

    fixed game manager and coins

---
+++ Scripts/GameManager copy.gd
@@ -0,0 +1,15 @@
+
+extends Node
+
+var score = 0
+
+#@onready var score_label = $Label
+
+func add_point():
+	score += 1
+	print(score)
+	#if score == 1:
+	#	score_label.text = "You collected " + str(score) + " coin!"
+	#else:
+	#	score_label.text = "You collected " + str(score) + " coins!"
+	
