b06c92a5acf2dd9071b7d306afe9e132ae403918
Andy <andyqulam@gmail.com>
2024-08-18 12:04:25 -0700

    reworked level

--- Scripts/Platform copy.gd
+++ Scripts/Platform copy.gd
@@ -10,8 +10,8 @@ var min_limit = 0
 @export var platSpeedMult = 1
 
 func _ready():
-	var max_lim = position.x + 100
-	var min_lim = position.x - 100
+	var max_lim = position.x + 80
+	var min_lim = position.x - 8
 	max_limit = max_lim
 	min_limit = min_lim
 	print(max_lim)
