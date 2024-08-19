65f31a450a0b957544a63e8e78a55608f564c210
Spunboing <diamondjellybean457@gmail.com>
2024-08-18 17:04:33 -0700

    added exit door to level 1

---
+++ Scripts/exit_door.gd
@@ -0,0 +1,15 @@
+extends Area2D
+@onready var complete = $complete
+@onready var timer = $Timer
+@onready var player = $"../Player"
+
+
+
+func _on_body_entered(body):
+	player.set_physics_process(false)
+	player.get_node("AnimatedSprite2D").play("jump")
+	complete.play()
+	timer.start()
+	
+func _on_timer_timeout():
+	get_tree().change_scene_to_file("res://Scenes/template_level.tscn")
