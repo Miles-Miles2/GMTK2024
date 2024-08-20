e0fdcee44e903618e57bff0dae673ca468f360bf
Spunboing <diamondjellybean457@gmail.com>
2024-08-19 22:24:08 -0700

    Audio Adujstments

--- Scripts/Button.gd
+++ Scripts/Button.gd
@@ -1,4 +1,12 @@
 extends Button
 
+@onready var audio_stream_player_2d = $AudioStreamPlayer2D
+@onready var timer = $Timer
+
 func _on_pressed():
+	timer.start()
+	audio_stream_player_2d.play()
+
+
+func _on_timer_timeout():
 	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
